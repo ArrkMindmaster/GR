cac94059b40aa129c1d53e7a959b1c234c48551d
ArrkMindmaster <35239563+ArrkMindmaster@users.noreply.github.com>
2022-08-31 18:16:04 -0400

    Initial commit
    Initial commit, with Program and Person classes

---
+++ GuaranteedRate/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuaranteedRate
+{
+	public class Person : IEnumerable, IComparable<Person>
+	{
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public string Email { get; set; }   //Any validation on this? Need to include @ and . in order?
+		public string FavoriteColor { get; set; }
+		public DateTime DateOfBirth { get; set; }
+		private PeopleSortOption SortOption;
+
+		public void SetSortOption(PeopleSortOption option)
+		{
+			SortOption = option;
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			return ((IEnumerable)LastName).GetEnumerator();
+		}
+
+		public int CompareTo(Person obj)
+		{
+			switch (SortOption)
+			{
+				case PeopleSortOption.ColorThenLastName:
+					int result = FavoriteColor.CompareTo(obj.FavoriteColor);
+					if(result==0)
+					{
+						result = LastName.CompareTo(obj.LastName);
+					}
+					return result;
+				case PeopleSortOption.BirthDate:
+					return DateOfBirth.CompareTo(obj.DateOfBirth);
+				case PeopleSortOption.LastNameDesc:
+					return LastName.CompareTo(obj.LastName) * -1; //Reverses normal sort order, for descending
+				default:
+					//This should never happen
+					throw new IndexOutOfRangeException($"{SortOption} is not valid as a sort order.");
+			}
+		}
+	}
+
+	public enum PeopleSortOption
+	{
+		None = 0,
+		ColorThenLastName = 1,
+		BirthDate = 2,
+		LastNameDesc = 3
+	}
+}
---
+++ GuaranteedRate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuaranteedRate
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Person> people = new List<Person>();
+
+			foreach(string filename in args)
+			{
+				AddFileData(filename, people);
+			}
+			OutputToConsole(people, PeopleSortOption.ColorThenLastName);
+			OutputToConsole(people, PeopleSortOption.BirthDate);
+			OutputToConsole(people, PeopleSortOption.LastNameDesc);
+		}
+	}
+}
